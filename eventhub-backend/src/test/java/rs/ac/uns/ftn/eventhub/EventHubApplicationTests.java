3d334f5489cbc0ec507352747d31229cd6d35def
flower0408 <natasakotaranin1@gmail.com>
2026-08-31 22:47:47 +0200

    Project started

---
+++ eventhub-backend/src/test/java/rs/ac/uns/ftn/eventhub/EventHubApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.eventhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
